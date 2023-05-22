f98ade99c93de734e558a1d2f525550ec5e338cf
Skrub <44077242+triplebproductionzz@users.noreply.github.com>
2023-05-22 14:46:32 -0600

    finished login and user ID across pages

---
+++ login.js
@@ -0,0 +1,10 @@
+function login() {
+    console.log("SDfsadfa");
+    const nameEl = document.getElementsByClassName("form-control name");
+    console.log(nameEl[0].value);
+    localStorage.setItem("userName", nameEl[0].value);
+    console.log(document.getElementById("changeUser"));
+    document.getElementById("changeUser").innerHTML = nameEl[0].value;
+    window.location.href = "choose.html";
+  }
+  
